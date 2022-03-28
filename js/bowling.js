64c87dccc0918576e23fe9308227333bff05485c
ZaidAG <72582743+ZaidAG@users.noreply.github.com>
2022-03-27 18:02:47 -0700

    Add files via upload

---
+++ js/bowling.js
@@ -0,0 +1,62 @@
+AFRAME.registerComponent("bowling-balls", {
+  init: function () {
+    this.throwBall();
+  },
+  throwBall: function () {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "z") {
+        var  ball = document.createElement("a-entity");
+
+        ball.setAttribute("gltf-model", "./assets/models/bowling_ball/scene.gltf");
+
+        ball.setAttribute("scale", { x: 4, y: 4,  z: 4});
+
+        var cam = document.querySelector("#camera");
+
+        pos = cam.getAttribute("position");
+
+        ball.setAttribute("position", {
+          x: pos.x,
+          y: pos.y-1.2,
+          z: pos.z,
+        });
+
+        var camera = document.querySelector("#camera").object3D;
+
+        //get the camera direction as Three.js Vector
+        var direction = new THREE.Vector3();
+        camera.getWorldDirection(direction);
+
+        //set the velocity and it's direction
+        ball.setAttribute("velocity", direction.multiplyScalar(-10));
+
+        var scene = document.querySelector("#scene");
+
+        ball.addEventListener("collide",this.removeBall)
+        scene.appendChild(ball);
+      }
+    });
+  },
+  removeBall:function(e){
+    console.log(e.detail.target.el)
+    console.log(e.detail.body.el)
+    var element=e.detail.target.el
+    var elementHit=e.detail.body.el
+    if(elementHit.id.includes('pin')){
+      elementHit.setAttribute('material',{
+        opacity:1,
+        transparent:true
+      })
+      var impulse=new CANNON.Vec3(-2,2,1)
+      var worldPoint=new CANNON.Vec3().copy(
+        elementHit.getAttribute("position")
+      )
+      elementHit.body.applyImpulse(impulse.worldPoint)
+      element.removeEventListener('collide',this.shoot)
+      var scene=document.querySelector('#scene')
+      scene.removeChild(element)
+    }
+  }
+});
+
+
